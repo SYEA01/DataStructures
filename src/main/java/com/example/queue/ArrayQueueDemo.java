c5e26f8c58cbf4a4ae2c5d897f659f02b3188eb9
taoao <taoao0101@163.com>
2024-03-28 17:34:11 +0800

    p13-数组模拟队列-测试

--- src/main/java/com/example/queue/ArrayQueueDemo.java
+++ src/main/java/com/example/queue/ArrayQueueDemo.java
@@ -1,10 +1,59 @@
 package com.example.queue;
 
+import java.util.Scanner;
+
 /**
  * 用数组模拟队列
  */
 public class ArrayQueueDemo {
     public static void main(String[] args) {
+        // 创建一个队列
+        ArrayQueue arrayQueue = new ArrayQueue(3);
+        char key = ' ';  // 接收用户输入
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        // 输出一个菜单
+        while (loop) {
+            System.out.println("s(show)：显示队列");
+            System.out.println("e(exit)：退出程序");
+            System.out.println("a(add)：添加数据到队列");
+            System.out.println("g(get)：从队列中取出数据");
+            System.out.println("h(head)：查看队列头的一个数据");
+            key = scanner.next().charAt(0);  // 接收一个字符
+            switch (key) {
+                case 's':
+                    arrayQueue.showQueue();
+                    break;
+                case 'a':
+                    System.out.print("请输入一个数字：");
+                    int value = scanner.nextInt();
+                    arrayQueue.addQueue(value);
+                    break;
+                case 'g':
+                    try {
+                        int result = arrayQueue.getQueue();
+                        System.out.println("取出的数据是：" + result);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'h':
+                    try {
+                        int head = arrayQueue.headQueue();
+                        System.out.println("队列头的数据是：" + head);
+                    } catch (RuntimeException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'e':
+                    scanner.close();
+                    loop = false;
+                default:
+                    break;
+            }
+        }
+        System.out.println("程序退出。");
+
 
     }
 }
@@ -52,7 +101,9 @@ class ArrayQueue {
             throw new RuntimeException("队列空，不能取数据");
         }
         front++;
-        return arr[front];
+        int get = arr[front];
+        arr[front] = 0;
+        return get;
     }
 
     // 显示队列的所有数据

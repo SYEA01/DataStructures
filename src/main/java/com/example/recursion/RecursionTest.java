66697a807ac7e9d5956836aa26fddc5aea54df20
taoao <taoao0101@163.com>
2024-04-09 10:35:08 +0800

    P43---递归案例-阶乘问题

--- src/main/java/com/example/recursion/RecursionTest.java
+++ src/main/java/com/example/recursion/RecursionTest.java
@@ -5,11 +5,13 @@ package com.example.recursion;
  */
 public class RecursionTest {
     public static void main(String[] args) {
-        test(5);
+//        test(5);
+        System.out.println("factorial(4) = " + factorial(4));
     }
 
     /**
      * 打印问题
+     *
      * @param n
      */
     public static void test(int n) {
@@ -17,5 +19,18 @@ public class RecursionTest {
             test(n - 1);
         }
         System.out.println("n = " + n);
+    }
+
+    /**
+     * 阶乘问题
+     * @param n
+     * @return
+     */
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return factorial(n - 1) * n;
+        }
     }
 }

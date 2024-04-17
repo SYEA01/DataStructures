33e777565072e4efd8b69908f6043b377f210e69
taoao <taoao0101@163.com>
2024-04-17 17:40:16 +0800

    P64-希尔排序（交换）测试耗时

--- src/main/java/com/example/sort/ShellSort.java
+++ src/main/java/com/example/sort/ShellSort.java
@@ -1,15 +1,32 @@
 package com.example.sort;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  * 希尔排序
+ * 排序前 = 2024-04-17 17:35:45
+ * 排序后 = 2024-04-17 17:35:50
+ * 80000 条数据耗时5秒
  */
 public class ShellSort {
     public static void main(String[] args) {
-        int[] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+//        int[] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+
+        //
+        int[] arr = new int[80000];
+        for (int i = 0; i < 80000; i++) {
+            arr[i] = (int) (Math.random() * 8000000);  // 生成 [0,800000) 的随机数
+        }
+        Date date1 = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("排序前 = " + sdf.format(date1));
+
         shellSort(arr);
 
+        Date date2 = new Date();
+        System.out.println("排序后 = " + sdf.format(date2));
     }
 
 
@@ -27,8 +44,6 @@ public class ShellSort {
             for (int i = gap; i < len; i++) {
                 // 遍历各组中所有的元素（共gap组 ）【 用于比较相邻的元素，并进行交换，直到找到正确的位置。 】
                 for (int j = i - gap; j >= 0; j -= gap) {
-                    System.out.println(j + "====>" + arr[j]);
-                    System.out.println(j + gap + "====>" + arr[j + gap]);
                     if (arr[j] > arr[j + gap]) {
                         temp = arr[j];
                         arr[j] = arr[j + gap];
@@ -36,7 +51,6 @@ public class ShellSort {
                     }
                 }
             }
-            System.out.println("希尔排序后 = " + Arrays.toString(arr));
         }
 
 
@@ -79,4 +93,6 @@ public class ShellSort {
 //        System.out.println("希尔排序第3轮 = " + Arrays.toString(arr));
 
     }
+
+
 }

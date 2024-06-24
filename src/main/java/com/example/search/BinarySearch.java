d2fd15ad1c71fba73a17742d285cf72048dae39f
taoao <taoao0101@163.com>
2024-06-24 18:03:06 +0800

    二分查找--找到所有的

--- src/main/java/com/example/search/BinarySearch.java
+++ src/main/java/com/example/search/BinarySearch.java
@@ -1,13 +1,16 @@
 package com.example.search;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 二分查找
  */
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 5, 6, 7, 8, 9};
-        int i = binarySearch(arr, 0, arr.length - 1, 4);
-        System.out.println("i = " + i);
+        int[] arr = {1, 2, 3, 3, 3, 3, 3, 5, 6, 7, 8, 9};
+        List<Integer> list = binarySearch2(arr, 0, arr.length - 1, 4);
+        System.out.println("list = " + list);
     }
 
     /**
@@ -33,4 +36,42 @@ public class BinarySearch {
             return mid;
         }
     }
+
+
+    public static List<Integer> binarySearch2(int[] arr, int left, int right, int target) {
+        List<Integer> list = new ArrayList<>();
+        if (left > right) {
+            return list;
+        }
+
+        int mid = (left + right) / 2;
+        if (arr[mid] < target) {  // 向右递归
+            binarySearch2(arr, mid + 1, right, target);
+        } else if (arr[mid] > target) { // 向左递归
+            binarySearch2(arr, left, mid - 1, target);
+        } else {
+            list.add(mid);
+            // 向左扫描
+            int temp = mid - 1;
+            while (true) {
+                if (temp < 0 || arr[temp] != target) {
+                    break;
+                }
+                list.add(temp);
+                temp--;
+            }
+
+            // 向右扫描
+            temp = mid + 1;
+            while (true) {
+                if (temp > arr.length - 1 || arr[temp] != target) {
+                    break;
+                }
+                list.add(temp);
+                temp++;
+            }
+        }
+        return list;
+    }
+
 }
